8582db05ed108f28b48a36a15565a14c274093b8
xweiba <lh1738289913@gmail.com>
2018-06-03 03:50:24 +0800

    重写随机数生成工具

--- src/main/java/com/jnshu/tools/RandNum.java
+++ src/main/java/com/jnshu/tools/RandNum.java
@@ -12,14 +12,25 @@ import java.util.Random;
 
 public class RandNum {
     public static String getRandLength(int intLength){
-        Random random = new Random();
-        // 获取随机数
-        double pross = (1 + random.nextDouble()) * Math.pow(10, intLength);
+        //35是因为数组是从0开始的，26个字母+10个数字
+        final int  maxNum = 36;
+        int i;  //生成的随机数
+        int count = 0; //生成的密码的长度
+        char[] str = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+                'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-        // 将获取的随机数转换为字符串
-        String fixLengthString = String.valueOf(pross);
+        StringBuffer pwd = new StringBuffer("");
+        Random r = new Random();
+        while(count < intLength){
+            //生成随机数，取绝对值，防止生成负数，
+            i = Math.abs(r.nextInt(maxNum));  //生成的数最大为36-1
 
-        // 返回固定长度随机数
-        return fixLengthString.substring(1, intLength+1);
+            if (i >= 0 && i < str.length) {
+                pwd.append(str[i]);
+                count ++;
+            }
+        }
+        return pwd.toString();
     }
 }
