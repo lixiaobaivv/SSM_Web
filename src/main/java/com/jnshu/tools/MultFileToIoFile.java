4f5e219a2748390aa5274d20e483432840596a80
xweiba <lh1738289913@gmail.com>
2018-06-06 04:52:00 +0800

    转换更加彻底, 直接转换为文件流

--- src/main/java/com/jnshu/tools/MultFileToIoFile.java
+++ src/main/java/com/jnshu/tools/MultFileToIoFile.java
@@ -2,8 +2,13 @@ package com.jnshu.tools;
 
 
 import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 /**
@@ -14,8 +19,17 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
  **/
 
 public class MultFileToIoFile {
-    public static DiskFileItem multipartFile(MultipartFile multipartFile){
+    private final static Logger LOGGER = LoggerFactory.getLogger(MultFileToIoFile.class);
+    public static InputStream multipartToInputStream(MultipartFile multipartFile){
         CommonsMultipartFile cf = (CommonsMultipartFile) multipartFile;
-        return (DiskFileItem) cf.getFileItem();
+        DiskFileItem diskFileItem = (DiskFileItem) cf.getFileItem();
+        try {
+            InputStream inputStream = diskFileItem.getInputStream();
+            return inputStream;
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.debug("文件流转换失败");
+        }
+        return null;
     }
 }
