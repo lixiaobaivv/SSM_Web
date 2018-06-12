361aa8ba3ed44eded21d1afbaa598d74a4056c2b
xweiba <lh1738289913@gmail.com>
2018-06-12 22:34:37 +0800

    取消毫秒秒转换, 解决JspTag 时间为空时的bug

--- src/main/java/com/jnshu/tools/JspTag.java
+++ src/main/java/com/jnshu/tools/JspTag.java
@@ -17,21 +17,23 @@ public class JspTag extends TagSupport {
 
     @Override
     public int doStartTag() throws JspException {
-        if (!value.equals("")) {
+        if (!value.trim().isEmpty()) {
             String vv = "" + value;
             try {
+                // logger.debug("vv: " + vv);
                 //将字符串转换为long, trim()除去空格,转换时不能有空格
                 long timeLong = Long.valueOf(vv.trim());
                 // logger.debug("Long 开始转换为 String : " + timeLong);
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                java.util.Date judt = new Date(timeLong * 1000);
+                java.util.Date judt = new Date(timeLong);
                 String s = simpleDateFormat.format(judt);
                 pageContext.getOut().write(s);
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
                 logger.error("转换失败");
             }
         } else {
+            logger.debug("vv test");
             /* 为空时返回空值,不处理 jsp会解析出错*/
             String s = "";
             try {
